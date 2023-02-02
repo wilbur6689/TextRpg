bc75a24d21f846a723e027fb0b52563c4887a632
wilbur6689 <wilbur6689@gmail.com>
2023-02-02 08:06:34 -0800

    initial commit

--- src/main/java/willis/learn/Main.java
+++ src/main/java/willis/learn/Main.java
@@ -2,6 +2,6 @@ package willis.learn;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello rpg world!");
     }
 }
